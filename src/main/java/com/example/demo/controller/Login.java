7936bcfa7ad964bce50dd5c4fbf5bfb062ad7e0e
Zhao <284578220@qq.com>
2019-08-04 23:16:02 +0800

    初次提交项目

---
+++ src/main/java/com/example/demo/controller/Login.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("login")
+public class Login {
+
+    @GetMapping("page")
+    public ModelAndView toLoginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
+}
